edf36ddf7d47778ff0011b43374305abeedf9666
Iziedking <128899752+Iziedking@users.noreply.github.com>
2025-06-26 13:38:04 +0100

    Update install-deps.sh

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-# === Update & Install System Dependencies ===
+
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y screen curl build-essential pkg-config libssl-dev git-all protobuf-compiler gawk bison gcc make wget tar
 
-# === Install Rust (required for prover) ===
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 rustup target add riscv32i-unknown-none-elf
 
-# === Build & Install GLIBC 2.39 (Safe method) ===
+
 mkdir -p /opt/glibc-2.39-src && cd /opt/glibc-2.39-src
 wget -c https://ftp.gnu.org/gnu/glibc/glibc-2.39.tar.gz
 tar -xvzf glibc-2.39.tar.gz
+
+e
+cd glibc-2.39
+sed -i 's/-Werror//g' Makeconfig
+
+
+cd ..
 mkdir glibc-build && cd glibc-build
 ../glibc-2.39/configure --prefix=/opt/glibc-2.39
 make -j$(nproc)
 sudo make install
 
-# === Install Nexus CLI ===
 cd ~
-curl https://cli.nexus.xyz/ | sh
+curl https://cli.nexus.xyz/ | sh -y
 source ~/.bashrc
