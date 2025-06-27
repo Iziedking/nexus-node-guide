6580165c8e4c564ab973a4165da8511c50cce62d
Iziedking <128899752+Iziedking@users.noreply.github.com>
2025-06-27 03:14:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
-echo "🛠 Installing Nexus Node CLI Environment..."
+set -e
 
-bash <(curl -s https://raw.githubusercontent.com/Iziedking/nexus-node-guide/main/scripts/install-deps.sh)
+echo " Starting Nexus Prover Setup..."
 
-echo "✅ Done! 
+
+echo " Installing system packages..."
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y screen curl build-essential pkg-config libssl-dev git-all protobuf-compiler
+
+
+echo " Installing Rust..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+
+source $HOME/.cargo/env
+
+echo " Adding riscv32i-unknown-none-elf target..."
+rustup target add riscv32i-unknown-none-elf
+
+
+echo " Installing Nexus CLI..."
+curl https://cli.nexus.xyz/ | sh
+
+
+echo " Reloading environment..."
+source ~/.bashrc
+
+echo "✅ Done! You can now run Nexus with:"
+echo "  nexus-network start --node-id <your-node-id>"
