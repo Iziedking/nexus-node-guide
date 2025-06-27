eb87bcfb9c8d26e142068c400cac1e4bc012a709
Iziedking <128899752+Iziedking@users.noreply.github.com>
2025-06-27 03:46:07 +0100

    Update run-nexus.sh

--- scripts/run-nexus.sh
+++ scripts/run-nexus.sh
@@ -1,44 +1,16 @@
 #!/bin/bash
 
-set -e  # Stop on error
-
-# === Variables ===
-GLIBC_VERSION="2.39"
-GLIBC_PREFIX="/opt/glibc-$GLIBC_VERSION"
-GLIBC_SRC="/opt/glibc-$GLIBC_VERSION-src"
-GLIBC_TARBALL="glibc-$GLIBC_VERSION.tar.gz"
-GLIBC_URL="https://ftp.gnu.org/gnu/glibc/$GLIBC_TARBALL"
-
-# === Download & Extract ===
-mkdir -p "$GLIBC_SRC"
-cd "$GLIBC_SRC"
-
-# Only download if it doesn't exist
-if [ ! -f "$GLIBC_TARBALL" ]; then
-    wget -c "$GLIBC_URL"
-fi
-
-# Only extract if the folder is not there
-if [ ! -d "glibc-$GLIBC_VERSION" ]; then
-    tar -xvzf "$GLIBC_TARBALL"
-fi
-
-# === Build & Install ===
-mkdir -p glibc-build
-cd glibc-build
-
-# Only configure if Makefile doesn’t exist
-if [ ! -f Makefile ]; then
-    ../glibc-$GLIBC_VERSION/configure --prefix="$GLIBC_PREFIX"
-fi
-
+mkdir -p /opt/glibc-2.39-src && cd /opt/glibc-2.39-src
+wget -c https://ftp.gnu.org/gnu/glibc/glibc-2.39.tar.gz
+tar -xvzf glibc-2.39.tar.gz
+mkdir glibc-build && cd glibc-build
+../glibc-2.39/configure --prefix=/opt/glibc-2.39
 make -j$(nproc)
 sudo make install
 
-# === Setup Nexus Execution ===
 export NEXUS_BIN=$(which nexus-network)
-export LOADER="$GLIBC_PREFIX/lib/ld-linux-x86-64.so.2"
-export GLIBCLIB="$GLIBC_PREFIX/lib"
+export LOADER="/opt/glibc-2.39/lib/ld-linux-x86-64.so.2"
+export GLIBCLIB="/opt/glibc-2.39/lib"
 export LIBGCC_PATH="/usr/lib/x86_64-linux-gnu"
 export FULL_LIB_PATH="$GLIBCLIB:$LIBGCC_PATH:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu"
 
