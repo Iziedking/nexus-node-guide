fcd48a6d702ef563c72f879325f37881b2e62f97
Iziedking <128899752+Iziedking@users.noreply.github.com>
2025-06-27 04:04:28 +0100

    Update run-nexus.sh

--- scripts/run-nexus.sh
+++ scripts/run-nexus.sh
@@ -8,10 +8,11 @@ mkdir glibc-build && cd glibc-build
 make -j$(nproc)
 sudo make install
 
+
 export NEXUS_BIN=$(which nexus-network)
 export LOADER="/opt/glibc-2.39/lib/ld-linux-x86-64.so.2"
 export GLIBCLIB="/opt/glibc-2.39/lib"
-export LIBGCC_PATH="/usr/lib/x86_64-linux-gnu"
+export LIBGCC_PATH="/usr/lib/gcc/x86_64-linux-gnu/11"
 export FULL_LIB_PATH="$GLIBCLIB:$LIBGCC_PATH:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu"
 
 $LOADER --library-path $FULL_LIB_PATH $NEXUS_BIN "$@"
