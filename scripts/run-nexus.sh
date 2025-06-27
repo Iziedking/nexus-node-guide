b77545fcd98e668e78167bb90e9287901b17ff66
Iziedking <128899752+Iziedking@users.noreply.github.com>
2025-06-27 03:35:34 +0100

    Update run-nexus.sh

--- scripts/run-nexus.sh
+++ scripts/run-nexus.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+mkdir -p /opt/glibc-2.39-src && cd /opt/glibc-2.39-src
+wget -c https://ftp.gnu.org/gnu/glibc/glibc-2.39.tar.gz
+tar -xvzf glibc-2.39.tar.gz
+mkdir glibc-build && cd glibc-build
+../glibc-2.39/configure --prefix=/opt/glibc-2.39
+make -j$(nproc)
+sudo make install
+
 export NEXUS_BIN=$(which nexus-network)
 export LOADER="/opt/glibc-2.39/lib/ld-linux-x86-64.so.2"
 export GLIBCLIB="/opt/glibc-2.39/lib"
